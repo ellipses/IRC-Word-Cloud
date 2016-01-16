a46ea231167b172b3c9e7909c4f40bc142f40031
ellipses <prasadt@tcd.ie>
2016-01-16 01:34:16 +0000

    basic structure of word cloud

--- irc.py
+++ irc.py
@@ -2,6 +2,11 @@
 
 import re
 import sys
+import scipy
+import random
+import numpy as ny
+from PIL import Image, ImageDraw, ImageFont
+from scipy import ndimage
 
 
 def return_stopword_list():
@@ -21,23 +26,29 @@ def return_stopword_list():
 def get_tuples(tuple):
     return tuple[-1]
 
-def form_dict(name, msg):
+def match_name(name, msg):
+    ##probably dont need two slashes
     time_stamp = '\\[\\d\\d:\\d\\d\\]'
     end_time_stamp = '(?:(?!\\[\\d\\d:\\d\\d\\]).)*'
     sentence = ' <(\w*)>([\w*\s*\d*]*)'
     user_name = '%s'%name
     
-    print 'searching for ' + user_name    
+    #print 'searching for ' + user_name    
     result = re.findall(time_stamp+sentence+end_time_stamp, msg)
-        
-    print 'adding to list'
+    #add error if list is of size 0    
+    #print 'adding to list'
     list = []
     for (name, sentence) in result:
         if re.match(user_name.lower(), name.lower()):
             list += sentence.split()
-    
+    #print list
+    return list
+
+#returns top 200 words and their frequency in a tuple    
+def get_freq(name, msg):    
+    list = match_name(name, msg)
     stop_words = return_stopword_list()    
-    print 'forming dict'
+    #print 'forming dict'
     dict = {}    
     for word in list:
         word = word.lower()
@@ -46,32 +57,76 @@ def form_dict(name, msg):
                 dict[word]+=1
             else:
                 dict[word] = 1
-            
+    #raise error if dictionary is of size 0        
     tuples = dict.items()
+            
+    sorted_tuple = sorted(tuples, key = get_tuples, reverse = True)
     
-    print tuples    
-    # file2 = open('result.txt','w')
-    # sorted_tuple = sorted(tuples, key = get_tuples, reverse = True)
+    return sorted_tuple[:max(50, len(sorted_tuple))]
     # for tuple in sorted_tuple:
         # line = ' '.join(str(x) for x in tuple)
         # file2.write(line + '\n')
     # file2.close()
     
-
-
+def word_cloud(frequency_list):
+    fontsize = 70
+    total = 0.0
+    for tuple in frequency_list:
+        total += tuple[1]
+        
+    relative_freq = [tuple[1]/total for tuple in frequency_list]
+    normalised = [val*(1/max(relative_freq)) for val in relative_freq]
+    words = [tuple[0] for tuple in frequency_list]
+    #print relative_freq
+    #print normalised
+    #print words
+        
+    status = False
+    while not status:
+        image = Image.new('L', (512,512), color = 0)
+        draw = ImageDraw.Draw(image)
+        fontlist = [fontsize*val for val in normalised]
+        iteration = 0
+        while(iteration<len(words)):
+            word = words[iteration]
+            fnt = ImageFont.truetype('Calibri.ttf', int(fontlist[iteration]))
+            size = fnt.getsize(word)
+            #bigger rectangle than bounding box to compensate
+            rect = (size[0]+40, size[1]+40)
+            result = scipy.ndimage.filters.uniform_filter(image, rect, mode='constant', cval = 255)
+            location = ny.argwhere(result==0)
+    
+            if len(location)==0:
+                break_image = image
+                break_rect = rect
+                fontsize-=5
+                fontsize
+                #break
+            else:
+                iteration+=1
+                rand_val = location[random.randint(0, len(location)-1)]
+                #adjusting the location to account for offset from kernel centre
+                value = (rand_val[0]-size[1]/2,rand_val[1]-size[0]/2)
+                draw.text(value[::-1], word, font=fnt, fill=255)
+            if word == words[-1]:
+                print fontsize
+                status=True
+    
+    image.show()
 
 # main function 
 def main():
     ##load file
     
-    #file = open('test2.txt','r')
-    #msg = file.read()
-    #file.close()
-    name = 'GoRy'   #make this lower case before passing it to the function.
+    file = open('small_irc.txt','r')
+    msg = file.read()
+    file.close()
+    name = 'reh'   #make this lower case before passing it to the function.
        
     #create and return dict
-    msg = '[17:36] <GorySnake> how ss how HOW how ss ! was your session ,, yesterday apollo\nI am dying lads\n[17:36] <GorySnake> dont forget to import data after you download it Cobelcog\n[17:36] <apollo> we won all but one game\n[17:37] <Cobelcog> how do i do that?'
-    form_dict(name, msg)
+    #msg = '[17:36] <GorySnake> how ss how HOW how ss ! was your session ,, yesterday apollo\nI am dying lads\n[17:36] <GorySnake> dont forget to import data after you download it Cobelcog\n[17:36] <apollo> we won all but one game\n[17:37] <Cobelcog> how do i do that?'
+    frequency_list = get_freq(name, msg)
+    word_cloud(frequency_list)
            
       
 
